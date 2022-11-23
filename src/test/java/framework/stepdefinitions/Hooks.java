38eb4b7ea8a30dab84ea3058a06db4f370f15b8e
Asiye Sarıbulak <5364542@ogrenci.bilecik.edu.tr>
2022-11-24 00:03:05 +0300

    hooks

---
+++ src/test/java/framework/stepdefinitions/Hooks.java
@@ -0,0 +1,26 @@
+package framework.stepdefinitions;
+
+import framework.utilities.Driver;
+import io.cucumber.java.Scenario;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class Hooks {
+
+    @Before
+    public void setUp(){
+
+    }
+
+    @After
+    public void tearDown(Scenario scenario){
+        final byte[] screenshot=((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+        if (scenario.isFailed()) {
+            scenario.attach(screenshot, "image/png","screenshots");
+        }
+        Driver.closeDriver();
+    }
+
+}
